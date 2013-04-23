c19df5eee5a4afb67d5f6d1e902502b1fb421ebb
André Santos <andreccdr@gmail.com>
2013-04-24 00:11:06 +0100

    Imagens Locais Corrigidas

---
+++ ArqueoDB/Controllers/LocaisController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ArqueoDB.Models;
+using ArqueoDB.DAL;
+
+namespace ArqueoDB.Controllers
+{
+    public class LocaisController : Controller
+    {
+        private EntidadesArqueoDB db = new EntidadesArqueoDB();
+
+        //
+        // GET: /Locais/
+
+        public ActionResult Index()
+        {
+            var locais = db.Locais.Include(l => l.Organizacao).Include(l => l.Responsavel).Include(l => l.Distrito);
+            return View(locais.ToList());
+        }
+
+        //
+        // GET: /Locais/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Local local = db.Locais.Find(id);
+            if (local == null)
+            {
+                return HttpNotFound();
+            }
+            return View(local);
+        }
+
+        //
+        // GET: /Locais/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.OrganizacaoID = new SelectList(db.Organizacoes, "OrganizacaoID", "Nome");
+            ViewBag.ResponsavelID = new SelectList(db.Profissionais, "ProfissionalID", "ProfissionalID");
+            ViewBag.DistritoID = new SelectList(db.Distritos, "DistritoID", "Nome");
+            return View();
+        }
+
+        //
+        // POST: /Locais/Create
+
+        [HttpPost]
+        public ActionResult Create(Local local)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Locais.Add(local);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.OrganizacaoID = new SelectList(db.Organizacoes, "OrganizacaoID", "Nome", local.OrganizacaoID);
+            ViewBag.ResponsavelID = new SelectList(db.Profissionais, "ProfissionalID", "ProfissionalID", local.ResponsavelID);
+            ViewBag.DistritoID = new SelectList(db.Distritos, "DistritoID", "Nome", local.DistritoID);
+            return View(local);
+        }
+
+        //
+        // GET: /Locais/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Local local = db.Locais.Find(id);
+            if (local == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.OrganizacaoID = new SelectList(db.Organizacoes, "OrganizacaoID", "Nome", local.OrganizacaoID);
+            ViewBag.ResponsavelID = new SelectList(db.Profissionais, "ProfissionalID", "ProfissionalID", local.ResponsavelID);
+            ViewBag.DistritoID = new SelectList(db.Distritos, "DistritoID", "Nome", local.DistritoID);
+            return View(local);
+        }
+
+        //
+        // POST: /Locais/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Local local)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(local).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.OrganizacaoID = new SelectList(db.Organizacoes, "OrganizacaoID", "Nome", local.OrganizacaoID);
+            ViewBag.ResponsavelID = new SelectList(db.Profissionais, "ProfissionalID", "ProfissionalID", local.ResponsavelID);
+            ViewBag.DistritoID = new SelectList(db.Distritos, "DistritoID", "Nome", local.DistritoID);
+            return View(local);
+        }
+
+        //
+        // GET: /Locais/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Local local = db.Locais.Find(id);
+            if (local == null)
+            {
+                return HttpNotFound();
+            }
+            return View(local);
+        }
+
+        //
+        // POST: /Locais/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Local local = db.Locais.Find(id);
+            db.Locais.Remove(local);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Publicar(int id)
+        {
+            Local local = db.Locais.Find(id);
+            local.Publico = true;
+            db.SaveChanges();
+
+            return Redirect(Request.UrlReferrer.AbsoluteUri);
+        }
+
+        public ActionResult Ocultar(int id)
+        {
+            Local local = db.Locais.Find(id);
+            local.Publico = false;
+            db.SaveChanges();
+
+            return Redirect(Request.UrlReferrer.AbsoluteUri);
+        }
+    }
+}
