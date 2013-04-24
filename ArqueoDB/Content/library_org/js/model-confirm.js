72efd410bc8d80a9567f7d103a78932529d0a697
André Santos <andreccdr@gmail.com>
2013-04-24 15:11:24 +0100

    Bootstrap Confirm e Começado Artefactos

---
+++ ArqueoDB/Content/library_org/js/model-confirm.js
@@ -0,0 +1,27 @@
+$('#modal-from-dom-ocultar').on('show', function() {
+    var id = $(this).data('id'),
+        removeBtn = $(this).find('.btn-danger');
+
+    removeBtn.attr('href', removeBtn.attr('href').replace(/IDLOCAL/, id));
+})
+
+$('.confirm-ocultar').on('click', function(e) {
+    e.preventDefault();
+
+    var id = $(this).data('id');
+    $('#modal-from-dom-ocultar').data('id', id).modal('show');
+});
+
+$('#modal-from-dom-publicar').on('show', function() {
+    var id = $(this).data('id'),
+        removeBtn = $(this).find('.btn-danger');
+
+    removeBtn.attr('href', removeBtn.attr('href').replace(/IDLOCAL/, id));
+})
+
+$('.confirm-publicar').on('click', function(e) {
+    e.preventDefault();
+
+    var id = $(this).data('id');
+    $('#modal-from-dom-publicar').data('id', id).modal('show');
+});
--- ArqueoDB/Controllers/DashLocalController.cs
+++ ArqueoDB/Controllers/DashLocalController.cs
@@ -1,10 +1,9 @@
 ﻿using ArqueoDB.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using ArqueoDB.DAL;
+using PagedList;
 
 namespace ArqueoDB.Controllers
 {
@@ -26,19 +25,69 @@ namespace ArqueoDB.Controllers
             return View(local);
         }
 
-        public ActionResult Artefactos(int id = 0)
+        public ActionResult Artefactos(int id, string sortOrder, string currentFilter, string searchString, string type, int? page)
         {
             Local local = db.Locais.Find(id);
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.CurrentType = type;
+
             if (local == null)
             {
                 return HttpNotFound();
             }
 
-            ViewData["Dashboard"] = "Local";
-            ViewData["Activo"] = "Artefactos";
+            if (Request.HttpMethod == "GET")
+            {
+                searchString = currentFilter;
+            }
+            else
+            {
+                page = 1;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var artefactos = from a in db.Artefactos where a.LocalID == local.LocalID select a;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                artefactos = artefactos.Where(l => l.Nome.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            switch (sortOrder)
+            {
+                case "Nome":
+                    artefactos = artefactos.OrderBy(l => l.Nome);
+                    break;
+                default:
+                    break;
+
+            }
+
+            switch (type)
+            {
+                case "Públicos":
+                    artefactos = artefactos.Where(l => l.Publico == true);
+                    break;
+                case "Ocultos":
+                    artefactos = artefactos.Where(l => l.Publico == false);
+                    break;
+                default:
+                    break;
+
+            }
+
+            int pageSize = 9;
+            int pageNumber = (page ?? 1);
+            ViewBag.Locais = artefactos.ToList().ToPagedList(pageNumber, pageSize);
+
+            ViewData["Dashboard"] = "Organizacao";
+            ViewData["Activo"] = "Locais";
 
             return View(local);
         }
+
 
         /* GET: Publicações */
         public ActionResult Publicacoes(int id = 0)
