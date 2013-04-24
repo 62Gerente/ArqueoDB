8a987c1c24f6da450408ff23e516e3cad6cc17ba
psfdc <pedrosfdcarneiro@gmail.com>
2013-04-24 01:50:58 +0100

    a desenvolver controlador para os documentos dos locais..

--- ArqueoDB/Controllers/DashLocalController.cs
+++ ArqueoDB/Controllers/DashLocalController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 using ArqueoDB.DAL;
 
 namespace ArqueoDB.Controllers
@@ -56,13 +57,67 @@ namespace ArqueoDB.Controllers
         }
 
         /* GET: Documentos */
-        public ActionResult Documentos(int id = 1)
+        public ActionResult Documentos(int id, string sortOrder, string currentFilter, string searchString, string type, int? page)
         {
             Local local = db.Locais.Find(id);
             if (local == null)
             {
                 return HttpNotFound();
             }
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.CurrentType = type;
+            
+            if (Request.HttpMethod == "GET")
+            {
+                searchString = currentFilter;
+            }
+            else
+            {
+                page = 1;
+            }           
+
+            ViewBag.CurrentFilter = searchString;
+            
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                local.Documentos.Where(l => l.Titulo.ToUpper().Contains(searchString.ToUpper()));                
+            }
+
+            IEnumerable<Documento> query = local.Documentos;
+            
+            switch (sortOrder)
+            {
+                case "Nome":
+                     query = local.Documentos.OrderBy(doc => doc.Titulo);
+                    break;
+                case "Data":
+                    query = local.Documentos.OrderBy(doc => doc.DataPublicacao);
+                    break;
+                case "Autor":
+                    query = local.Documentos.OrderBy(doc => doc.Responsavel.Utilizador.Nome);
+                    break;
+                default:
+                    break;
+            }
+
+            switch (type)
+            {
+                case "Públicos":
+                    query = query.Where(doc => doc.Publico == true);
+                    break;
+                case "Ocultos":
+                    query = query.Where(doc => doc.Publico == false);
+                    break;
+                default:
+                    break;
+
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            ViewBag.Locais = query.ToList().ToPagedList(pageNumber, pageSize);
 
             ViewData["Dashboard"] = "Local";
             ViewData["Activo"] = "Documentos";
