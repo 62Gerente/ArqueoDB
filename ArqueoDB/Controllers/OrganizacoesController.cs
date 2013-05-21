d09d87a44859681cc63a20ac366da06d7a55f01f
Daniel Carvalho <dapcarvalho@gmail.com>
2013-05-21 18:22:12 +0100

    fix

--- ArqueoDB/Controllers/OrganizacoesController.cs
+++ ArqueoDB/Controllers/OrganizacoesController.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
@@ -161,4 +162,143 @@ namespace ArqueoDB.Controllers
             return Redirect(Request.UrlReferrer.AbsoluteUri);
         }
     }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ArqueoDB.Models;
+using ArqueoDB.DAL;
+
+namespace ArqueoDB.Controllers
+{
+    public class OrganizacoesController : Controller
+    {
+        private EntidadesArqueoDB db = new EntidadesArqueoDB();
+
+        //
+        // GET: /Organizacoes/
+
+        public ActionResult Index()
+        {
+            var organizacoes = db.Organizacoes.Include(o => o.Distrito).Include(o => o.Responsavel).Include(o => o.ImagemPerfil).Include(o => o.ImagemCapa);
+            return View(organizacoes.ToList());
+        }
+
+        //
+        // GET: /Organizacoes/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Organizacao organizacao = db.Organizacoes.Find(id);
+            if (organizacao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organizacao);
+        }
+
+        //
+        // GET: /Organizacoes/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.DistritoID = new SelectList(db.Distritos, "DistritoID", "Nome");
+            ViewBag.ResponsavelID = new SelectList(db.Profissionais, "ProfissionalID", "ProfissionalID");
+            ViewBag.ImagemPerfilID = new SelectList(db.Imagens, "ImagemID", "Nome");
+            ViewBag.ImagemCapaID = new SelectList(db.Imagens, "ImagemID", "Nome");
+            return View();
+        }
+
+        //
+        // POST: /Organizacoes/Create
+
+        [HttpPost]
+        public ActionResult Create(Organizacao organizacao)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Organizacoes.Add(organizacao);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DistritoID = new SelectList(db.Distritos, "DistritoID", "Nome", organizacao.DistritoID);
+            ViewBag.ResponsavelID = new SelectList(db.Profissionais, "ProfissionalID", "ProfissionalID", organizacao.ResponsavelID);
+            ViewBag.ImagemPerfilID = new SelectList(db.Imagens, "ImagemID", "Nome", organizacao.ImagemPerfilID);
+            ViewBag.ImagemCapaID = new SelectList(db.Imagens, "ImagemID", "Nome", organizacao.ImagemCapaID);
+            return View(organizacao);
+        }
+
+        //
+        // GET: /Organizacoes/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Organizacao organizacao = db.Organizacoes.Find(id);
+            if (organizacao == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DistritoID = new SelectList(db.Distritos, "DistritoID", "Nome", organizacao.DistritoID);
+            ViewBag.ResponsavelID = new SelectList(db.Profissionais, "ProfissionalID", "ProfissionalID", organizacao.ResponsavelID);
+            ViewBag.ImagemPerfilID = new SelectList(db.Imagens, "ImagemID", "Nome", organizacao.ImagemPerfilID);
+            ViewBag.ImagemCapaID = new SelectList(db.Imagens, "ImagemID", "Nome", organizacao.ImagemCapaID);
+            return View(organizacao);
+        }
+
+        //
+        // POST: /Organizacoes/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Organizacao organizacao)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(organizacao).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DistritoID = new SelectList(db.Distritos, "DistritoID", "Nome", organizacao.DistritoID);
+            ViewBag.ResponsavelID = new SelectList(db.Profissionais, "ProfissionalID", "ProfissionalID", organizacao.ResponsavelID);
+            ViewBag.ImagemPerfilID = new SelectList(db.Imagens, "ImagemID", "Nome", organizacao.ImagemPerfilID);
+            ViewBag.ImagemCapaID = new SelectList(db.Imagens, "ImagemID", "Nome", organizacao.ImagemCapaID);
+            return View(organizacao);
+        }
+
+        //
+        // GET: /Organizacoes/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Organizacao organizacao = db.Organizacoes.Find(id);
+            if (organizacao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organizacao);
+        }
+
+        //
+        // POST: /Organizacoes/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Organizacao organizacao = db.Organizacoes.Find(id);
+            db.Organizacoes.Remove(organizacao);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+>>>>>>> fix
 }
