f75c3dfe2f49b85fd6a3460d144e937b8b6b167e
Daniel Carvalho <dapcarvalho@gmail.com>
2013-06-02 20:01:15 +0100

    Homepage 100%

--- ArqueoDB/Controllers/HomeController.cs
+++ ArqueoDB/Controllers/HomeController.cs
@@ -18,9 +18,35 @@ namespace ArqueoDB.Controllers
         public ActionResult Index()
         {
             List<Local> locs = new List<Local>();
-            foreach (Local l in db.Locais) { locs.Add(l); }
-
+            List<Publicacao> listapub = new List<Publicacao>();
+            Dictionary<Publicacao, String> pubdetails = new Dictionary<Publicacao, String>();
+            Dictionary<Publicacao, String> publicacoes = new Dictionary<Publicacao, String>();
+            foreach (Local l in db.Locais.Where(l => l.Publico && !l.Apagado)) { locs.Add(l); }
+            locs.OrderByDescending(l => l.Comentarios.Count());
             ViewBag.locals = locs;
+
+            foreach (Local loc in locs)
+            {
+                listapub.AddRange(loc.Publicacoes);
+                foreach (Publicacao pbl in loc.Publicacoes.Where(p => p.Publico && !p.Apagado))
+                {
+                    publicacoes.Add(pbl, loc.Nome);
+                    pubdetails.Add(pbl, loc.Imagens.First().Directoria.Caminho + loc.Imagens.First().Nome);
+                }
+
+            }
+            listapub = listapub.OrderByDescending(p => p.DataPublicacao.ToShortDateString()).ThenByDescending(p => p.DataPublicacao.ToShortTimeString()).ToList();
+            ViewBag.publicacoes = publicacoes;
+            ViewBag.pubdetails = pubdetails;
+            ViewBag.listapub = listapub;
+
+
+
+
+
+
+
+
             return View();
         }
 
